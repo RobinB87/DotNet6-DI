a53055c256bd47011da38c6075e75061711acdf1
Robin Bette <robinbette@hotmail.com>
2022-12-02 08:18:11 +0100

    demonstration of how easy unit testing can be with DI

---
+++ PeopleViewer.Presentation.Tests/PeopleViewModelTests.cs
@@ -0,0 +1,31 @@
+﻿namespace PeopleViewer.Presentation.Tests;
+
+[TestClass]
+public class PeopleViewModelTests
+{
+    [TestMethod]
+    public void People_OnRefreshPeople_IsPopulated()
+    {
+        var reader = new FakeReader();
+        var vm = new PeopleViewModel(reader);
+
+        vm.RefreshPeople();
+
+        Assert.IsNotNull(vm.People);
+        Assert.AreEqual(2, vm.People.Count());
+    }
+
+    [TestMethod]
+    public void People_OnClearPeople_IsEmpty()
+    {
+        var reader = new FakeReader();
+        var vm = new PeopleViewModel(reader);
+        vm.RefreshPeople();
+        Assert.AreNotEqual(0, vm.People.Count(), 
+            "Invalid arrange");
+
+        vm.ClearPeople();
+
+        Assert.AreEqual(0, vm.People.Count());
+    }
+}
