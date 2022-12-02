0d460371c657bca7fdb9f75cfb399c536f55e4b2
Robin Bette <robinbette@hotmail.com>
2022-12-02 08:37:31 +0100

    use property injection to override FileLoader in unit test

--- PersonDataReader.CSV.Tests/CSVReaderTests.cs
+++ PersonDataReader.CSV.Tests/CSVReaderTests.cs
@@ -13,4 +13,26 @@ public class CSVReaderTests
 
         Assert.AreEqual(2, result.Count());
     }
+
+    [TestMethod]
+    public void GetPeople_WithNoFile_ThrowsFileNotFoundException()
+    {
+        var reader = new CSVReader();
+        // If FileLoader not overridden by property injection,
+        // The StreamReader in the LoadFile method
+        // will look for actual file on system
+        Assert.ThrowsException<FileNotFoundException>(
+            () => reader.GetPeople());
+    }
+
+    [TestMethod]
+    public void GetPeople_WithSomeBadRecords_ReturnsGoodRecords()
+    {
+        var reader = new CSVReader();
+        reader.FileLoader = new FakeFileLoader("Mixed");
+
+        var result = reader.GetPeople();
+
+        Assert.AreEqual(2, result.Count());
+    }
 }
